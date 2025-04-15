aed4ad176dc2f9e1b79f6ede24625a287e6f38ba
opada <opadasadleh@gmail.com>
2025-04-15 22:55:30 +0300

    add your commit message

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,74 @@
 import { Link } from "expo-router";
-import { Text, ScrollView, View, Image } from "react-native";
+import { Text, ScrollView, View, Image, ActivityIndicator, FlatList } from "react-native";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
-
+import SearchBar from "@/components/SearchBar"; // Adjust the path as needed
+import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard"; // Adjust the path as needed
 export default function Index() {
+    const router = useRouter(); //first time use hook (use mean hooks)
+
+    const {
+      data:movies ,
+      loading : moviesLoading ,
+      error: moviesError } = useFetch(() => fetchMovies({
+      query :'',
+    }))
+
   return (
     <View className="flex-1 bg-primary ">
       <Image source={images.bg} className="absolute w-full z-0"  />
 
       <ScrollView className="flex -1 px-5" 
-      showsVerticalScrollIndicator={false}>
+      showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%" ,paddingBottom:10}}>
     <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+      {moviesLoading ? (
+        <ActivityIndicator 
+          size="large"
+          color="#0000ff"
+          className="mt-10 self-center"
+        />
+      
+      ):moviesError ? (
+        <Text>Error: {moviesError?.message}</Text>
+      ):(
+      <View className="flex-1 mt-5">
+        <SearchBar   
+          onPress={() => router.push("/search")}
+          
+          placeholder="Search for movie "
+        />
+
+        <>
+          <Text className="text-lg text-white font-bold mt-5 mb-3" >Latest Movies</Text>
+        </>
+
+        <FlatList
+          data={movies}
+          renderItem={({item })=> (
+            <MovieCard 
+              {...item}
+            /> 
+          )}
+          keyExtractor={ (item) => item.id.toString() }
+          numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: 'flex-start',
+          gap: 20,
+          paddingRight: 5,
+          marginBottom: 0,
+        }}
+        className="mt-2 pd-32"
+        scrollEnabled={false}
+        />
+      </View>
+      )
+      }
+
+      
       </ScrollView>
     </View>
   );
---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View, Image,TextInput } from 'react-native'
+import React from 'react'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
+
+interface Props {
+    placeholder :string;
+    onPress?: () => void;
+    value: string; 
+    onChangeText: (text: string) => void;
+}
+
+const SearchBar = ( {placeholder,onPress,onChangeText, value} : Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-1">
+      <Image source={icons.search} className="size-5 " resizeMode="contain" tintColor="#ab8bff" />
+      <TextInput 
+        onPress={onPress }
+        placeholder={placeholder}
+        value ={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({})
